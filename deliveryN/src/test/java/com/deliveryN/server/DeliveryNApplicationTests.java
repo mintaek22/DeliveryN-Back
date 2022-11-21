01675c17d67d500e74cec0238c990af67198cda8
mintaek <88085338+mintaek22@users.noreply.github.com>
2022-11-21 23:33:52 +0900

    build: start

---
+++ deliveryN/src/test/java/com/deliveryN/server/DeliveryNApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deliveryN.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryNApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
