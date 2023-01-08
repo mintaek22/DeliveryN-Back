1e8030a679fb45839867a69aa54e718199fc166a
mintaek22 <biba99@naver.com>
2023-01-08 21:46:03 +0900

    feat: api exception

--- src/main/java/com/deliveryN/server/exception/ApiExceptionController.java
+++ src/main/java/com/deliveryN/server/exception/ApiExceptionController.java
@@ -1,14 +1,10 @@
 package com.deliveryN.server.exception;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.TypeMismatchException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import java.net.BindException;
 
 
 @RestControllerAdvice
